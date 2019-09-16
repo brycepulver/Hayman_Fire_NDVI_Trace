bc7a1491d76fc1c8576829a1157734ff78063a6c
brycepulver <brycepulver@live.com>
2019-09-16 17:31:09 -0600

    finishing the assignment

--- assignment_1.R
+++ assignment_1.R
@@ -25,7 +25,6 @@ ndsi <- read_csv(files[2]) %>%
 ndvi <- read_csv(files[3])%>% 
   rename(burned=2,unburned=3) %>%
   mutate(data='ndvi')
-
 # Stack as a tidy dataset
 full_long <- rbind(ndvi,ndmi,ndsi) %>%
   gather(key='site',value='value',-DateTime,-data) %>%
@@ -64,7 +63,22 @@ ggplot(summer_only,aes(x=ndvi,y=ndmi,color=site)) +
 
 
 ## Your code here
+snow_cover <- filter (full_wide, month %in% c(1:4)) %>%
+  group_by(site,year) %>%
+  summarize(mean_ndsi=mean(ndsi))
+  
 
+veg_growth <- filter (full_wide, month %in% c(5:8)) %>%
+  group_by(site,year) %>%
+  summarize(mean_ndvi=mean(ndvi))
+
+SC_VG <- merge(snow_cover,veg_growth) 
+ 
+
+ggplot(SC_VG,aes(x=mean_ndsi,y=mean_ndvi, color = site)) +
+  geom_point()
+
+head(SC_VG)
 ## End code for question 2 -----------------
 
 
@@ -73,12 +87,59 @@ ggplot(summer_only,aes(x=ndvi,y=ndmi,color=site)) +
 # and burned and unburned? 
 
 ## Your code here
+Sc_VG_pre_post <- SC_VG %>% 
+          mutate(treatment = cut(year,breaks=c(0,2003,2020),
+                         labels=c('pre-burn','post-burn')))%>%
+          group_by(year,site,treatment)
+  
+
+ggplot(Sc_VG_pre_post,aes(x=mean_ndsi,y=mean_ndvi,color=site)) +
+  geom_point() + 
+  theme_few() + 
+  scale_color_few() + 
+  theme(legend.position=c(0.4,0.2)) + 
+  facet_wrap(~treatment)
 
 ## End code for question 3
 
 ###### Question 4 #####
 #What month is the greenest month on average? Does this change in the burned
-# plots after the fire? 
+#plots after the fire? 
 
+ndvi_month_pre_post <- full_long %>% 
+  mutate(year = year(DateTime),
+         month = month(DateTime),
+         treatment = cut(year,breaks=c(0,2003,2020),
+                         labels=c('pre-burn','post-burn')))%>%
+group_by(month,site,treatment,data)%>%
+  filter(str_detect(data,"ndvi"))%>%
+  summarize(mean_ndvi = mean(value))
+
+ggplot(ndvi_month_pre_post,aes(x=month,y=mean_ndvi,color=treatment)) +
+  geom_point() + 
+  geom_line() +
+  theme_few() + 
+  scale_color_few() + 
+  theme(legend.position=c(0.6,0.2)) + 
+  facet_wrap(~site)
+
+view(ndvi_month_pre_post)
 ##### Question 5 ####
 #What month is the snowiest on average?
+
+ndsi_month_pre_post <- full_long %>% 
+  mutate(year = year(DateTime),
+         month = month(DateTime),
+         treatment = cut(year,breaks=c(0,2003,2020),
+                         labels=c('pre-burn','post-burn')))%>%
+  group_by(month,site,treatment,data)%>%
+  filter(str_detect(data,"ndsi"))%>%
+  summarize(mean_ndsi = mean(value))
+
+ggplot(ndsi_month_pre_post,aes(x=month,y=mean_ndsi,color=treatment)) +
+  geom_point() + 
+  geom_line() +
+  theme_few() + 
+  scale_color_few() + 
+  theme(legend.position=c(0.6,0.2)) + 
+  facet_wrap(~site)
